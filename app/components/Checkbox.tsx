995bce99fcde83a122f96f15d609dddd976df2a1
Ilya Gavrikov <me@engi.io>
2024-03-30 14:58:46 +0400

    add focus interaction to custom checkbox input

--- app/components/Checkbox.tsx
+++ app/components/Checkbox.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
+import styles from "./checkbox.module.css";
 
 type CheckboxProps = {
   name?: string;
@@ -10,6 +11,10 @@ type CheckboxProps = {
 
 export const Checkbox = (props: CheckboxProps) => {
   const [checked, setChecked] = useState(props.value ?? props.defaultValue ?? false);
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(e.currentTarget.checked);
+  }, []);
 
   useEffect(() => {
     if (props.value !== undefined) {
@@ -24,7 +29,7 @@ export const Checkbox = (props: CheckboxProps) => {
   return (
     <div className="relative w-6 h-6">
       <div
-        className={`relative flex justify-center items-center w-6 h-6 border border-solid rounded-md ${checked ? 'border-blue-700 bg-blue-700' : 'border-gray-300 bg-white'}`}
+        className={`${styles.checkbox} relative flex justify-center items-center w-6 h-6 border border-solid rounded-md ${checked ? 'border-blue-700 bg-blue-700' : 'border-gray-300 bg-white'}`}
       >
         {checked && <div className="bg-white w-3 h-3 rounded-sm" />}
       </div>
@@ -32,7 +37,7 @@ export const Checkbox = (props: CheckboxProps) => {
         type="checkbox"
         name={props.name}
         checked={checked}
-        onChange={(e) => setChecked(e.currentTarget.checked)}
+        onChange={handleChange}
         className="absolute inset-0 opacity-0 cursor-pointer"
       />
     </div>
