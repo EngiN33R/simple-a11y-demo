fd5d37ed17e77b9ef8b2662e987ae54d689b618e
Ilya Gavrikov <me@engi.io>
2024-03-30 14:43:19 +0400

    use native html interactions for checkbox

--- app/components/Checkbox.tsx
+++ app/components/Checkbox.tsx
@@ -17,29 +17,24 @@ export const Checkbox = (props: CheckboxProps) => {
     }
   }, [props.value]);
 
-  const onToggle = useCallback(() => {
-    setChecked((cur) => !cur);
-  }, []);
-
   useEffect(() => {
     props.onChange?.(checked);
   }, [checked]);
 
   return (
     <div className="relative w-6 h-6">
+      <div
+        className={`relative flex justify-center items-center w-6 h-6 border border-solid rounded-md ${checked ? 'border-blue-700 bg-blue-700' : 'border-gray-300 bg-white'}`}
+      >
+        {checked && <div className="bg-white w-3 h-3 rounded-sm" />}
+      </div>
       <input
         type="checkbox"
         name={props.name}
         checked={checked}
-        onChange={(e) => console.log('native checkbox change')}
-        className="absolute inset-0 opacity-0"
+        onChange={(e) => setChecked(e.currentTarget.checked)}
+        className="absolute inset-0 opacity-0 cursor-pointer"
       />
-      <div
-        className={`relative flex justify-center items-center w-6 h-6 border border-solid rounded-md cursor-pointer ${checked ? 'border-blue-700 bg-blue-700' : 'border-gray-300 bg-white'}`}
-        onClick={onToggle}
-      >
-        {checked && <div className="bg-white w-3 h-3 rounded-sm" />}
-      </div>
     </div>
   )
 };
